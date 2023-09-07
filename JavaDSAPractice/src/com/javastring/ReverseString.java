e3de996bc76bce60eb71fda91c4f6d0c0de972fe
hardiknasare <nasare.hardik@gmail.com>
2023-09-08 00:51:42 +0530

    Java reverse string

---
+++ JavaDSAPractice/src/com/javastring/ReverseString.java
@@ -0,0 +1,27 @@
+package com.javastring;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the string to be reverse  : ");
+		String str = sc.next();
+		
+		StringBuilder sb = new StringBuilder(str).reverse();
+		
+		System.out.println("reversed == "+sb);
+		
+		
+		// Using for loop 
+		String reversedString = ""; 
+		for(int i = str.length()-1 ; i >= 0 ; i-- ) {
+			reversedString += str.charAt(i);
+		}
+		
+		System.out.println("reversed == "+reversedString);
+	}
+
+}
--- MyJavaDSAPractise.md
+++ MyJavaDSAPractise.md
@@ -1,1 +1,4 @@
 ## My Java DSA Practise started from Dt. 8 Sept 2023
+
+
+1) Dt 8/9 - Reverse a String
