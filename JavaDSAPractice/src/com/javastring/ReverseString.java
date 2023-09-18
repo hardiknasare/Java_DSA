48cc1dfa18b6359e2ad72e91995b9db4d080bae2
hardiknasare <nasare.hardik@gmail.com>
2023-09-18 22:46:35 +0530

    adding Anagram Check

--- JavaDSAPractice/src/com/javastring/ReverseString.java
+++ JavaDSAPractice/src/com/javastring/ReverseString.java
@@ -22,6 +22,8 @@ public class ReverseString {
 		}
 		
 		System.out.println("reversed == "+reversedString);
+		
+		sc.close();
 	}
 
 }
--- MyJavaDSAPractise.md
+++ MyJavaDSAPractise.md
@@ -3,3 +3,4 @@
 
 1) Dt 8/9 - Reverse a String
 2) Dt 11/9 - Palindrome Check
+3) Dt 11/9 - Anagram Check
